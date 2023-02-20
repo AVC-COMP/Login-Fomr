cec8fe0bcb6f00089a8177f20711e5070e8f9e1a
AVC-COMP <112658148+AVC-COMP@users.noreply.github.com>
2023-02-20 01:34:13 -1200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+//import logo from './lincolnlogo.jpeg';
+import './App.css';
+import Login from './Components/Login';
+import './App.css';
+alert('Welcome To Lincoln')
+
+function App() {
+  return (
+    <div className="App">
+     <Login/>
+    </div>
+    
+  );
+}
+
+export default App;
